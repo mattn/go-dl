97b5b16b4040f10404ae35ef51d64261c37ff098
mattn <mattn.jp@gmail.com>
2010-09-01 16:50:32 +0900

    example for windows

---
+++ example_windows.go
@@ -0,0 +1,25 @@
+package main
+
+import "dl"
+import "unsafe"
+
+func clen(n []byte) int {
+  for i := 0; i < len(n); i++ {
+    if n[i] == 0 {
+      return i
+    }
+  }
+  return len(n)
+}
+
+func main() {
+  handle, _ := dl.Open("libc.so")
+  proc, _ := dl.Sym(handle, "getenv")
+  ret := dl.Call(
+    proc,
+    uintptr(unsafe.Pointer(&([]byte)("HOME")[0])),
+    0,
+    0)
+  bytes := ((*[255]byte)(unsafe.Pointer(ret)))
+  println(string(bytes[0:clen(bytes)]))
+}
