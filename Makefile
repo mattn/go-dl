002f4596e65198c993da4f7cee944b2198f5712b
mattn <mattn.jp@gmail.com>
2010-09-01 16:48:32 +0900

    windows matter.

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@ include $(GOROOT)/src/Make.inc
 
 TARG     = dl
 ifeq ($(GOOS),windows)
-CGOFILES = dl_windows.go
+GOFILES = dl_windows.go
 else
 CGOFILES = dl_unix.go
 CGO_LDFLAGS = -ldl
--- dl_windows.go
+++ dl_windows.go
@@ -1,25 +1,7 @@
 package dl
 
-/*
-#include <windows.h.h>
-
-unsigned long dlcall(unsigned long p, unsigned long a1, unsigned long a2, unsigned long a3) {
-  typedef unsigned long (*type_call)(unsigned long, unsigned long, unsigned long);
-  type_call pcall = (type_call)p;
-  return pcall(a1,a2,a3);
-}
-*/
-import "C"
 import "os"
-import "unsafe"
-
-/*
-const (
-  RTLD_LAZY = C.RTLD_LAZY
-  RTLD_NOW = C.RTLD_NOW
-  RTLD_GLOBAL = C.RTLD_GLOBAL
-)
-*/
+import "syscall"
 
 func Open(filename string) (uintptr, os.Error) {
   ret, err := syscall.LoadLibrary(filename)
@@ -30,7 +12,7 @@ func Open(filename string) (uintptr, os.Error) {
 }
 
 func Sym(handle uintptr, symbol string) (uintptr, os.Error) {
-  ret, err := syscall.GetProcAddress(handle, symbol)
+  ret, err := syscall.GetProcAddress(uint32(handle), symbol)
   if err == 0 {
     return uintptr(ret), nil
   }
@@ -38,15 +20,11 @@ func Sym(handle uintptr, symbol string) (uintptr, os.Error) {
 }
 
 func Close(handle uintptr) os.Error {
-  syscall.FreeLibrary(handle)
+  syscall.FreeLibrary(uint32(handle))
   return nil
 }
 
 func Call(p, a1, a2, a3 uintptr) uintptr {
-  ret := C.dlcall(
-    C.ulong(p),
-    C.ulong(a1),
-    C.ulong(a2),
-    C.ulong(a3))
-  return uintptr(ret)
+  ret, _, _ := syscall.Syscall(p, a1, a2, a3)
+  return ret;
 }
